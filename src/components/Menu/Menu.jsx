3e8e0195b03513e4fa246da06ea4c2b409cbfb19
wojbuk3335 <wojbuk3335@gmail.com>
2026-04-22 20:14:00 +0200

    Add components

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import styles from './Menu.module.css'
+
+function Menu(){
+    return(
+        <div >
+            <ul className={styles.menu}>
+                <li className={styles.menuItem}>
+                    <a href="#">Home</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu
